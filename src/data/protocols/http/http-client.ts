03d3895c2b0e5d2e9eaba25dc89868b2bcfb8dd5
Hugo Oliveira <you@example.com>
2021-09-15 19:03:47 -0300

    feat: add http protocol

---
+++ src/data/protocols/http/http-client.ts
@@ -0,0 +1,19 @@
+import { HttpStatusCode } from './HttpStatusCode'
+
+export interface HttpClient<R = any> {
+  request: (data: HttpRequest) => Promise<HttpResponse<R>>
+}
+
+export type HttpRequest = {
+  url: string
+  method: HttpMethod
+  body?: any
+  headers?: any
+}
+
+export type HttpMethod = 'post' | 'get' | 'put' | 'delete'
+
+export type HttpResponse<T = any> = {
+  statusCode: HttpStatusCode
+  body?: T
+}
